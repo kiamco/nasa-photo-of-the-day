a55d9293cf1f9d1d01593dfc4affc52d00dbe0eb
kiamco <kiamcoo@gmail.com>
2019-08-08 20:55:37 -0700

    added more cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect }from "react";
-import ApodImage from "./components/apodCard"
-import ApodDescription from "./components/apodDescription";
+
 import Axios from "axios";
 import "./App.css"
 import ApodList from './components/apodList'
@@ -8,19 +7,14 @@ import ApodList from './components/apodList'
 
 function App() {
 
-  // const [apodImg, setApodImg] = useState();
-  // const [title, setTitle] = useState();
-  // const [description, setDescription] = useState();
-  const [dateSpan, setDateSpan] = useState("&start_date=2017-07-05&end_date=2017-07-10");
+  const [dateSpan, setDateSpan] = useState("&start_date=2017-06-20&end_date=2017-07-10");
   const [apodData, setApodData] = useState([]);
+
     useEffect(() => {
       Axios.get(`https://api.nasa.gov/planetary/apod?api_key=FpyUbGqSAHs5TkQWHCAZ3SJ1Cj5wnu5N1d9VKgjw${dateSpan}`)
         .then(response => {
-          console.log(response.data);
-          // const {hdurl, title , explanation} = response.data 
-          // setApodImg(hdurl);
-          // setTitle(title);
-          // setDescription(explanation)
+          console.log("Success",response.data);
+
           setApodData(response.data);
         })
         .catch(err => console.log(`App.js/ err: ${err}`))
