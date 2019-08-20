5250ab5614a94a26e58b55a667c4588816467472
kiamco <kiamcoo@gmail.com>
2019-08-19 20:51:37 -0700

    another one

--- src/components/apodDescription.js
+++ src/components/apodDescription.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import Icons from "./icons";
 import Styled from "styled-components";
 
-function Description(props){
-    
+function Description(props) {
+
     const [show, setShow] = useState(false);
-    const ReadMore = Styled.button`
+    const ReadMore = Styled.button `
         border:1px solid black;
         background:white;
         font-style: italic;
@@ -19,18 +19,20 @@ function Description(props){
             background: #F0F0F0;
         }
     `
-        
-    return(
-        <div className="img-description">
-            <h1>{props.imgName}</h1>
-            <ReadMore onClick={() => setShow(show === false ? true : false)}>description</ReadMore>
-            {show && <p className='description'>{props.imgDescription}</p> }
-            {/* <p className='description'>{show && props.imgDescription}</p> */}
-            {/* add like icon and other */}
-            <Icons />
-        </div>
-      
-    );
-}
 
-export default Description;
+    return ( <
+        div className = "img-description" >
+        <
+        h1 > { props.imgName } < /h1> <
+        ReadMore onClick = {
+            () => setShow(show === false ? true : false) } > description < /ReadMore> {
+            show && < p className = 'description' > { props.imgDescription } < /p> } { /* <p className='description'>{show && props.imgDescription}</p> */ } { /* add like icon and other */ } <
+                Icons / >
+                <
+                /div>
+
+
+        );
+    }
+
+    export default Description;
